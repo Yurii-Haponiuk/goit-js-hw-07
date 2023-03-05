069cea3e283784d528e611f7dd6bd0a18f239b75
Yurii-Haponiuk <yurii.gaponyuk@gmail.com>
2023-03-05 14:57:20 +0200

    Finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,11 +3,25 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
 
+function createGalleryItemsMarkup(items) {
+    return items.map(({ description, original, preview }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join("");
+}
+
+// console.log(itemsMarkup)
 
 
-
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 
